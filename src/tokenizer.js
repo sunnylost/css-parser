1e925ba8a8852f8d6a784b3d7654aaa52621ffb3
sunnylost <sunnylost@gmail.com>
2016-01-03 22:58:38 +0800

    finish consumeComment()

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,5 +1,5 @@
 let //https://drafts.csswg.org/css-syntax-3/#tokenization
-    TOKEN_TYPE        = {
+    TOKEN_TYPE           = {
         EOF:                  -2,
         COMMENT:              -1,
         IDENT:                0,
@@ -26,26 +26,49 @@ let //https://drafts.csswg.org/css-syntax-3/#tokenization
         COLON:                21, //:
         SEMICOLON:            22, //;
         COMMA:                23, //,
-        OPEN_SQUARE_BRACKET:  24, //[
-        CLOSE_SQUARE_BRACKET: 25, //]
-        OPEN_PARENTHESIS:     26, //(
-        CLOSE_PARENTHESIS:    27, //)
-        OPEN_BRACE:           28, //{
-        CLOSE_BRACE:          29  //}
+        LEFT_SQUARE_BRACKET:  24, //[
+        RIGHT_SQUARE_BRACKET: 25, //]
+        LEFT_PARENTHESIS:     26, //(
+        RIGHT_PARENTHESIS:    27, //)
+        LEFT_BRACE:           28, //{
+        RIGHT_BRACE:          29  //}
     },
 
-    NULL              = null,
-    CT                = 0x9, //CHARACTER TABULATION
-    LF                = 0xa,  //LINE FEED
-    FF                = 0xc, //FORM FEED
-    CR                = 0xd, //CARRIAGE RETURN
-    SPACE             = 0x20,
-    ASTERISK          = 0x2a, //*
-    SOLIDUS           = 0x2f,
-    NEWLINE           = LF,
+    NULL                 = null,
+    CT                   = 0x9,//CHARACTER TABULATION
+    LF                   = 0xa,//LINE FEED
+    FF                   = 0xc,//FORM FEED
+    CR                   = 0xd,//CARRIAGE RETURN
+    SPACE                = 0x20,
+    QUOTATION_MARK       = 0x22,//"
+    NUMBER_SIGN          = 0x23,//#
+    DOLLAR_SIGN          = 0x24,//$
+    APOSTROPHE           = 0x27,//'
+    LEFT_PARENTHESIS     = 0x28,//(
+    RIGHT_PARENTHESIS    = 0x29,//)
+    ASTERISK             = 0x2a,//*
+    PLUS_SIGN            = 0x2b,//+
+    COMMA                = 0x2c,//,
+    HYPHEN_MINUS         = 0x2d,//-
+    FULL_STOP            = 0x2e,//.
+    SOLIDUS              = 0x2f,///
+    COLON                = 0x3a,//:
+    SEMICOLON            = 0x3b,//;
+    LESS_THAN_SIGN       = 0x3c,//<
+    COMMERCIAL_AT        = 0x40,//@
+    LEFT_SQUARE_BRACKET  = 0x5b,//[
+    REVERSE_SOLIDUS      = 0x5c,//\
+    RIGHT_SQUARE_BRACKET = 0x5d,//]
+    CIRCUMFLEX_ACCENT    = 0x5e,//^
+    LOW_LINE             = 0x5f,//_
+    LEFT_CURLY_BRACKET   = 0x7b,//{
+    RIGHT_CURLY_BRACKET  = 0x7d,//}
+    VERTICAL_LINE        = 0x7c,//|
+    TILDE                = 0x7e,//~
+    NEWLINE              = LF,
 
     //https://drafts.csswg.org/css-syntax-3/#input-preprocessing
-    preprocessHanlder = ( cur, next ) => {
+    preprocessHanlder    = ( cur, next ) => {
         let curCP  = cur.codePointAt( 0 ),
             nextCP = next.codePointAt( 0 )
 
@@ -60,7 +83,15 @@ let //https://drafts.csswg.org/css-syntax-3/#tokenization
         return cur
     },
 
-    isWhitespace      = cp => cp === NEWLINE || cp === CT || cp === SPACE
+    isDigit              = cp => cp >= 0x30 && cp <= 0x39,
+    isWhitespace         = cp => cp === NEWLINE || cp === CT || cp === SPACE,
+    isUppercase          = cp => cp >= 0x41 && cp <= 0x5a,
+    isLowercase          = cp => cp >= 0x61 && cp <= 0x7a,
+    isLetter             = cp => isLowercase( cp ) || isUppercase( cp ),
+    isNonASCII           = cp => cp >= 0x80,
+    isNameStart          = cp => isLetter( cp ) || isNonASCII( cp ) || cp === LOW_LINE,
+    //TODO: reverse slash
+    isName               = cp => isNameStart( cp ) || isDigit( cp ) || cp === HYPHEN_MINUS
 
 class Tokenizer {
     constructor( src ) {
@@ -87,24 +118,26 @@ class Tokenizer {
     advance() {
         let ch, cp, token
 
-        if ( ch = this.next() ) {
+        if ( ch = this.peek() ) {
             cp = ch.codePointAt( 0 )
 
-            token = this.consumeComment()
-
-            if ( token ) {
+            if ( token = this.consumeComment() ) {
                 return token
+            }
+
+            if ( isNameStart( cp ) ) {
+                return this.consumeIdent()
             }
 
             switch ( cp ) {
                 case NEWLINE:
                 case CT:
                 case SPACE:
-                    token = this.consumeWhitespace()
-                    break
-            }
+                    return this.consumeWhitespace()
 
-            return token
+                case QUOTATION_MARK:
+                    return this.consumeString()
+            }
         }
 
         return NULL
@@ -137,24 +170,25 @@ class Tokenizer {
 
         if ( p1 && p2 &&
             p1.codePointAt( 0 ) === SOLIDUS &&
-            p2.codePointAt( 2 ) === ASTERISK
+            p2.codePointAt( 0 ) === ASTERISK
         ) {
             start = this.pos
-            comment.push( [ SOLIDUS, ASTERISK ] )
+            comment.push( p1, p2 )
             this.pos += 2
 
             while ( 1 ) {
                 p1 = this.peek( 1 )
                 p2 = this.peek( 2 )
                 if ( !p1 || !p2 ||
-                    p1.codePointAt( 0 ) !== SOLIDUS ||
-                    p2.codePointAt( 2 ) !== ASTERISK ) {
+                    ( p1.codePointAt( 0 ) !== ASTERISK &&
+                    p2.codePointAt( 0 ) !== SOLIDUS ) ) {
                     comment.push( this.next() )
+                } else {
+                    break
                 }
-                break
             }
 
-            comment.push( [ ASTERISK, SOLIDUS ] )
+            comment.push( p1, p2 )
             this.next( 2 )
             end   = this.pos
             value = comment.join( '' )
@@ -183,6 +217,30 @@ class Tokenizer {
                    start, end,
             type:  TOKEN_TYPE.WHITESPACE,
             value: whitespace.join( '' )
+        }
+    }
+
+    consumeString() {
+    }
+
+    consumeNumber() {
+    }
+
+    consumeIdent() {
+        let result = [],
+            start  = this.pos,
+            ch, end
+
+        while ( ( ch = this.peek() ) && isName( ch.codePointAt( 0 ) ) ) {
+            result.push( this.next() )
+        }
+
+        end = this.pos
+
+        return {
+                   start, end,
+            type:  TOKEN_TYPE.IDENT,
+            value: result.join( '' )
         }
     }
 }
