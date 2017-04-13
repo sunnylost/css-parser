9c404d1a6eb8ac24f235f67aefcde42ff9dc6f88
sunnylost <sunnylost@gmail.com>
2017-04-14 00:07:23 +0800

    update

--- src/tokenType.js
+++ src/tokenType.js
@@ -1,35 +1,35 @@
 //https://drafts.csswg.org/css-syntax-3/#tokenization
-export default {
-    EOF:                  -2,
-    COMMENT:              -1,
-    IDENT:                0,
-    FUNCTION:             1,
-    AT_KEYWORD:           2,
-    Hash:                 3,
-    STRING:               4,
-    BAD_STRING:           5,
-    URL:                  6,
-    BAD_URL:              7,
-    DELIM:                8,
-    NUMBER:               9,
-    PERCENTAGE:           10,
-    DIMENSION:            11,
-    INCLUDE_MATCH:        12,
-    DASH_MATCH:           13,
-    PREFIX_MATCH:         14,
-    SUFFIX_MATCH:         15,
-    SUBSTRING_MATCH:      16,
-    COLUMN:               17,
-    WHITESPACE:           18,
-    CDO:                  19,
-    CDC:                  20,
-    COLON:                21, //:
-    SEMICOLON:            22, //;
-    COMMA:                23, //,
-    LEFT_SQUARE_BRACKET:  24, //[
-    RIGHT_SQUARE_BRACKET: 25, //]
-    LEFT_PARENTHESIS:     26, //(
-    RIGHT_PARENTHESIS:    27, //)
-    LEFT_BRACE:           28, //{
-    RIGHT_BRACE:          29  //}
-}
+let types     = Object.create( null ),
+    typeNames = [
+        'EOF', 'COMMENT', 'IDENT', 'FUNCTION', 'AT_KEYWORD', 'Hash', 'STRING', 'BAD_STRING',
+        'URL',
+        'BAD_URL',
+        'DELIM',
+        'NUMBER',
+        'PERCENTAGE',
+        'DIMENSION',
+        'INCLUDE_MATCH',
+        'DASH_MATCH',
+        'PREFIX_MATCH',
+        'SUFFIX_MATCH',
+        'SUBSTRING_MATCH',
+        'COLUMN',
+        'WHITESPACE',
+        'CDO',
+        'CDC',
+        'COLON', //:
+        'SEMICOLON', //;
+        'COMMA', //,
+        'LEFT_SQUARE_BRACKET', //[
+        'RIGHT_SQUARE_BRACKET', //]
+        'LEFT_PARENTHESIS', //(
+        'RIGHT_PARENTHESIS', //)
+        'LEFT_BRACE', //{
+        'RIGHT_BRACE'  //}
+    ]
+
+typeNames.forEach( ( type ) => {
+    types[ type ] = type
+} )
+
+export default types
